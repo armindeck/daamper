794a76ad9c5460b6823700b942ac4641681c045c
armindeck <dbhsoficial2003@gmail.com>
2025-03-30 23:50:08 -0500

    Se quito la llamada a la versión

--- app/database/publicaciones/pu_bienvenida.php
+++ app/database/publicaciones/pu_bienvenida.php
@@ -1,5 +1,5 @@
-<?php #26/12/2024 - 4:22pm
-$ACR = ['creador' => 'normal', 'pubo' => 'publicacion', 'db_archivo' => 'pu_bienvenida.php', 'db_ruta' => 'bienvenida.php', 'fecha_publicado' => '18/12/2024 - 10:45pm', 'fecha_modificado' => '26/12/2024 - 4:22pm', 'id_publicador' => '1', 'creador_borrador_version' => '0.1.2 Beta', 'creador_borrador_fecha' => '26.12.2024', ];
+<?php #30/03/2025 - 10:52pm
+$ACR = ['creador' => 'normal', 'pubo' => 'publicacion', 'db_archivo' => 'pu_bienvenida.php', 'db_ruta' => 'bienvenida.php', 'fecha_publicado' => '18/12/2024 - 10:45pm', 'fecha_modificado' => '30/03/2025 - 10:52pm', 'id_publicador' => '1', 'creador_borrador_version' => '0.1.3 Estable', 'creador_borrador_fecha' => '30.03.2025', ];
 $AC = ['titulo' => 'Bienvenida!', 'descripcion' => 'Página web para ForoLink, Juegos, Animes, Proyectos, Blogs con múltiples temas personalizados, Panel administrativo avanzado con muchas funciones para la personalización de la página, extensiones y muchas otras cosas.', 'meta_descripcion' => 'Página web para ForoLink, Juegos, Animes, Proyectos, Blogs con múltiples temas personalizados, Panel administrativo avanzado con muchas funciones para la personalización de la página, extensiones y muchas otras cosas.', 'catalogo' => 'Hey!', 'meta_etiquetas' => 'Bienvenida', 'fragmento' => 'Página web para ForoLink, Juegos, Animes, Proyectos, Blogs con múltiples temas personalizados, Panel administrativo avanzado con muchas funciones para la personalización de la página, extensiones y muchas otras cosas.', 'contenido' => 'daamper es una página web para múltiples tipos de contenidos creados por Armin el día 15 de julio del 2024 como un proyecto personal para subir juegos, blogs, forolink, animes y muchos otros más.
 
 <a class="boton-2" target="_blank" href="https://dbproject.rf.gd/web/daamper">Ultima actualización <i class="fas fa-history"></i></a>
@@ -7,11 +7,6 @@ $AC = ['titulo' => 'Bienvenida!', 'descripcion' => 'Página web para ForoLink, J
 <details open><summary>Redes sociales</summary>
   <section class="flex-column botones-2 botones-mini">
     [Return=InputEnlace name="redes-sociales" contenedor="1" elemento="1"]
-  </section>
-</details>
-<details><summary>Release</summary>
-  <section>
-    [Return=FileReturnVersion src="version.txt" salto="true" espacio="true"]
   </section>
 </details>', 'tipo' => 'blog', 'miniatura' => 'assets/img/miniatura.png', 'miniatura_url' => '', 'anuncio' => 'on', 'ruta' => '', 'archivo' => 'bienvenida.php', 'directorio' => './', ];
 ?>
