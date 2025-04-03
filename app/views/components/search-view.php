7826934d24f0535668f2291ba8fecfaa4423b9a6
armindeck <dbhsoficial2003@gmail.com>
2025-04-02 22:02:24 -0500

    Componente de busqueda

---
+++ app/views/components/search-view.php
@@ -0,0 +1,12 @@
+<label>
+  <input type="checkbox" hidden>
+  <a><i class="fa fa-search"></i></a>
+  <div class="sub">
+    <form class="formulario flex-column" action="<?= $Web['directorio'] ?>search<?= $Web['config']['php'] ?>" method="get">
+      <span><?= Language('search') ?>:</span>  
+      <input class="campo" type="search" name="q" minlength="4" maxlength="255" value="<?= isset($_GET['q']) ? SCRIPTS->normalizar2($_GET['q']) : '' ?>" placeholder="<?= Language('search') ?>" required>
+      <input type="hidden" name="search-by" value="title-tags">
+      <input class="boton" type="submit" value="&#xf002 <?= Language('search') ?>">
+    </form>
+  </div>
+</label>
