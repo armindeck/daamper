d0a4a021bdfe62903ff02a95e474885ec57ee6ba
armindeck <dbhsoficial2003@gmail.com>
2025-04-02 22:04:43 -0500

    Se agrego el titulo con multiples lenguages para la busqueda

--- panel/app/creador/creadores/normal/mod.php
+++ panel/app/creador/creadores/normal/mod.php
@@ -1,7 +1,9 @@
 <?php
-if ($AC['ruta'].$AC['archivo'] == 'error.php') {
+if (in_array($AC['ruta'].$AC['archivo'], ['error.php', 'search.php'])) {
     $MOD = ['titulo'];
-    $AC['titulo'] = strtolower($AC['titulo']) != 'undefined' ? $AC['titulo'] : Language(['error', 'title'], 'posts');
+    $AC['titulo'] = strtolower($AC['titulo']) != 'undefined' ? $AC['titulo'] : 
+    ($AC['ruta'].$AC['archivo'] == 'error.php' ? Language(['error', 'title'], 'posts') :
+    Language('search'));
 }
 if ($AC['ruta'].$AC['archivo'] == 'panel/panel.php') {
     $MOD = ['titulo'];
