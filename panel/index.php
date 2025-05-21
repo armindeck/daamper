c66a81a801e486807b9556a4f1697416432ec477
armindeck <dbhsoficial2003@gmail.com>
2025-05-21 10:14:27 -0500

    El directorio panel fue eliminado, ahora se usara el directorio admin y se cambiaron los respectivos nombres por: index.php & admin.php

--- panel/index.php
+++
@@ -1,4 +0,0 @@
-<?php #19/12/2024 - 11:36pm
-$Web = ['directorio'=>'../','ruta'=>'panel/index.php'];
-require_once "{$Web["directorio"]}app/control/control.php";
-?>
--- panel/panel.php
+++
@@ -1,4 +0,0 @@
-<?php # 24/06/2024 ~ 2:11am -> 1
-$Web = ['directorio'=>'../','ruta'=>'panel/panel.php'];
-require_once $Web['directorio'].'app/control/control.php';
-?>
