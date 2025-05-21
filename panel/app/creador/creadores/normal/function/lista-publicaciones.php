53a99633b017ee20b6a0a23cedb51f8657348ad8
armindeck <dbhsoficial2003@gmail.com>
2025-05-21 10:32:51 -0500

    Fue eliminado y ahora se usara: database/creator/list-of-entries.json

--- panel/app/creador/creadores/normal/function/lista-publicaciones.php
+++
@@ -1,5 +0,0 @@
-<?php return [
-['entrada' => '', 'poster' => '', 'titulo' => 'Agregados recientemente', 'titulo-alternativo' => 'Publicaciones'],
-['entrada' => 'blog', 'poster' => '', 'titulo' => 'Blog', 'titulo-alternativo' => ''],
-['entrada' => 'post', 'poster' => '', 'titulo' => 'Posteos', 'titulo-alternativo' => 'Agregados recientemente'],
-];
