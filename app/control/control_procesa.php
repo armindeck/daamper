e6ece9f2d438ec4ca0864319f9b6b335917b8528
armindeck <dbhsoficial2003@gmail.com>
2025-05-21 10:12:33 -0500

    El directorio control fue eliminado, ahora se usara el directorio controller y los respectivos nombres: controller.php & admin.php

--- app/control/control_procesa.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Copyright (c) 2025 daamper
-// Licencia: Uso No Transferible - Prohibida la venta del código.
-// Para más detalles, consulta el archivo LICENSE.txt en la raíz del proyecto.
-
-$CONTROL_PROCESA = TRUE; require_once $Web['directorio'].'app/controller/controller.php';
