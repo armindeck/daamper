a2043368c05d134ac0a421832089b789f08f6340
armindeck <dbhsoficial2003@gmail.com>
2025-05-21 10:44:06 -0500

    Fueron movidos y se cambiaron los nombres al ingles: app/views/admin/

--- panel/app/comentarios/view.php
+++
@@ -1,6 +0,0 @@
-<section class="panel" style="padding: 0 4px 4px 4px;">
-	<section class="form"><strong><?= Language('comments') ?></strong></section>
-</section>
-<section style="padding: 0 4px 4px 4px;">
-	<?php FormComentario(true) ?>
-</section>
