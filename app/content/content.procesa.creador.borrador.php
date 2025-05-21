b90c39133a156528d4d04aa0c5f1603326965e96
armindeck <dbhsoficial2003@gmail.com>
2025-05-21 10:10:40 -0500

    Se cambio el nombre a creator.php

--- app/content/content.procesa.creador.borrador.php
+++
@@ -1,5 +0,0 @@
-<?php require_once __DIR__ . '/adaptabilidad.php';
-if(!file_exists("{$Web['directorio']}panel/app/creador/creadores/{$ACR_FORM['creador']}/{$ACR_FORM['creador']}.php")){
-    die(Language('creator-not-found', 'alert', ['value' => "<strong>{$ACR_FORM['creador']}</strong>"]));
-}
-$AXR = $ACR_FORM; $AX = $AC_FORM; ?>
