3611b3c53538b9a0dcc58a4a50c69260a28b4e2b
armindeck <dbhsoficial2003@gmail.com>
2025-05-27 13:39:25 -0500

    Adaptado para que funcione con los archivos json

--- app/content/content.php
+++ app/content/content.php
@@ -1,24 +1,29 @@
 <?php require_once __DIR__ . '/adaptabilidad.php';
-$ruta_publicaciones = $Web['directorio'].'app/database/publicaciones/pu_' . SCRIPTS->eslasToGuion($Web['ruta']);
-if(file_exists($ruta_publicaciones)){ require_once $ruta_publicaciones;
+$ruta_publicaciones_json = str_replace(".php", ".json", SCRIPTS->eslasToGuion($Web['ruta']));
+$ruta_publicaciones = RAIZ . 'database/post/' . $ruta_publicaciones_json;
+if(file_exists($ruta_publicaciones)){
+	$ACR = DATA->Post($ruta_publicaciones_json)["ACR"] ?? [];
+	$AC = DATA->Post($ruta_publicaciones_json)["AC"] ?? [];
 	if(!isset($ACR['creador'])){ die(Language('file-no-identifier', 'alert')); }
 } else {
 	if(substr($Web['ruta'], 0, 2) == 'p/'){
-		$ruta_perfil = $Web['directorio'].'app/database/publicaciones/pu_p-perfil.php';
-		if(file_exists($ruta_perfil)){ require_once $ruta_perfil; }
-			else { die(Language('profile-data-missing', 'alert')); }
+		$ruta_perfil = RAIZ . 'database/post/p-perfil.json';
+		if(file_exists($ruta_perfil)){
+			$ACR = DATA->Post("p-perfil")["ACR"] ?? [];
+			$AC = DATA->Post("p-perfil")["AC"] ?? [];
+			} else { die(Language('profile-data-missing', 'alert')); }
 	} else { die(Language('data-file-missing', 'alert')); }
 }
 
-$ruta_creadores = "{$Web['directorio']}panel/app/creador/creadores/{$ACR['creador']}/";
-if(!file_exists("$ruta_creadores{$ACR['creador']}.php")){ die(Language('creator-not-found', 'alert', ['value' => "<strong>{$ACR['creador']}</strong>"])); }
+if(!file_exists(RAIZ . "app/views/admin/creators/{$ACR['creador']}-view.php")){ die(Language('creator-not-found', 'alert', ['value' => "<strong>{$ACR['creador']}</strong>"])); }
 
-if(file_exists("$ruta_creadores/mod.php")){ require "$ruta_creadores/mod.php";
+$ruta_creadores_mod = RAIZ . "app/actions/admin/content/global/creators/mod/{$ACR['creador']}.php";
+if(file_exists($ruta_creadores_mod)){
+	require $ruta_creadores_mod;
 	if(isset($ACMOD)){ foreach ($ACMOD as $key => $value) { $AC[$key] = $value; } }
 }
 
 foreach ($AC as $key => $value) { $AX[$key] = $value; } $AXR = $ACR;
 
-unset($ruta_creadores); unset($ruta_publicaciones); unset($ruta_perfil);
+unset($ruta_creadores_mod); unset($ruta_publicaciones); unset($ruta_publicaciones_json); unset($ruta_perfil);
 unset($ACR); unset($AC); unset($ACMOD);
-?>
