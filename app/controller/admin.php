f4b1e3a20bfb9212b969ef4277c6ed4d4f60638c
armindeck <dbhsoficial2003@gmail.com>
2025-05-27 13:50:00 -0500

    Controlador

---
+++ app/controller/admin.php
@@ -0,0 +1,6 @@
+<?php
+// Copyright (c) 2025 daamper
+// Licencia: Uso No Transferible - Prohibida la venta del código.
+// Para más detalles, consulta el archivo LICENSE.txt en la raíz del proyecto.
+
+$PROCESS_ADMIN = TRUE; require_once __DIR__.'/controller.php';
--- app/controller/controller.php
+++ app/controller/controller.php
@@ -1,12 +1,17 @@
-<?php #21/06/2024 ~ 10:39pm
+<?php # 21/06/2024 ~ 10:39pm ~ Create file
+// Copyright (c) 2025 daamper
+// Licencia: Uso No Transferible - Prohibida la venta del código.
+// Para más detalles, consulta el archivo LICENSE.txt en la raíz del proyecto.
 session_start();
 require_once __DIR__.'/recursos.php';
 require_once __DIR__.'/system.php';
-require_once $Web['directorio'].AppScripts();
+require_once __DIR__.'/../scripts/scripts.php';
+require_once __DIR__.'/folder.php';
+require_once __DIR__.'/../model/model.php';
 require_once __DIR__.'/portabilidad.php';
 
-if (!isset($CONTROL_PROCESA)) {
-	foreach ([AppHttp(), AppContent(), AppDatabase(), AppViews()] as $value) {
-		require_once $Web['directorio'] . $value;
+if (!isset($PROCESS_ADMIN)) {
+	foreach ([AppGlobal(), AppContent(), AppViews()] as $value) {
+		require_once RAIZ . $value;
 	}
 }
