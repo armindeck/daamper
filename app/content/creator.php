814c8e72dbf4ba942afe95723dc47b3df1c0f1d7
armindeck <dbhsoficial2003@gmail.com>
2025-05-27 13:39:50 -0500

    Se cambio la ruta de la vista del creador

---
+++ app/content/creator.php
@@ -0,0 +1,5 @@
+<?php require_once __DIR__ . '/adaptabilidad.php';
+if(!file_exists(RAIZ . "app/views/admin/creators/{$ACR_FORM['creador']}-view.php")){
+    die(Language('creator-not-found', 'alert', ['value' => "<strong>{$ACR_FORM['creador']}</strong>"]));
+}
+$AXR = $ACR_FORM; $AX = $AC_FORM; ?>
