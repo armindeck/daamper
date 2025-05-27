b0b072b8513c5b28053d9959ff3b8757d518a040
armindeck <dbhsoficial2003@gmail.com>
2025-05-27 13:43:01 -0500

    Ahora se lanzara una alerta cuando se valla a cerrar sesión, se cambio mensajeSpan() y la ruta de los temas

--- app/global/get.php
+++ app/global/get.php
@@ -1,21 +1,18 @@
 <?php # CERRAR SESIÓN
-if(isset($_GET['cerrar-sesion']) && isset($_SESSION['id'])){
+if(isset($_GET['cerrar-sesion']) && $_GET['cerrar-sesion'] === "true" && isset($_SESSION['id'])){
 	unset($_SESSION['id']); unset($_SESSION['rol']); unset($_SESSION['cambiar_contrasena']);
-	mensajeSpan(['bg'=>'green',
-		'text'=> Language('farewell', 'alert'),
-		'ruta'=>"{$Web['directorio']}auth/iniciar{$Web['config']['php']}"
-	]);
+	sendAlert->Success(Language('farewell', 'alert'), "{$Web['directorio']}auth/login{$Web['config']['php']}");
 }
 # TEMA
 if (isset($_GET['tema']) && !empty($_GET['tema'])) {
-    if (file_exists($Web['directorio'] . AssetsCss('theme/'.SCRIPTS->normalizar2($_GET['tema'])))) {
+    if (file_exists($Web['directorio'] . AssetsCss('template/daamper/theme/'.SCRIPTS->normalizar2($_GET['tema'])))) {
         $_SESSION['tmp']['tema'] = SCRIPTS->normalizar2($_GET['tema']);
     }
 }
 
 # IDIOMAS
 if (isset($_GET['language']) && !empty($_GET['language'])) {
-    if (isset(Database('config/language')['global']['languages-options'][htmlspecialchars($_GET['language'])])) {
+    if (isset(DATA->Config('language')['global']['languages-options'][htmlspecialchars($_GET['language'])])) {
         $_SESSION['tmp']['language'] = htmlspecialchars($_GET['language']);
     }
 }
