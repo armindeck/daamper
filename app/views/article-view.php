8ec99c0c57855d731c0820e00f06e8c029718d1e
armindeck <dbhsoficial2003@gmail.com>
2025-05-27 13:47:45 -0500

    Se cambio anuncios por ads

--- app/views/article-view.php
+++ app/views/article-view.php
@@ -1,5 +1,5 @@
-<?php if(isset($Web['anuncios']['mostrar_anuncio_miniatura_article']) && !empty($Web['anuncios']['mostrar_anuncio_miniatura_article'])) { global $Web; ?>
-	<a class="anuncio-article-miniatura" target="_blank" href="<?= $Web['anuncios']['enlace_anuncio_miniatura_article'] ?? '' ?>">
-		<img style="max-width: 100%;" src="<?= $Web['config']['https_imagen']; ?><?= $Web['anuncios']['imagen_anuncio_miniatura_article'] ?? '' ?>">
+<?php if(isset($Web['ads']['mostrar_anuncio_miniatura_article']) && !empty($Web['ads']['mostrar_anuncio_miniatura_article'])) { global $Web; ?>
+	<a class="anuncio-article-miniatura" target="_blank" href="<?= $Web['ads']['enlace_anuncio_miniatura_article'] ?? '' ?>">
+		<img style="max-width: 100%;" src="<?= $Web['config']['https_imagen']; ?><?= $Web['ads']['imagen_anuncio_miniatura_article'] ?? '' ?>">
 	</a>
 <?php } ?>
