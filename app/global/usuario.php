55dc89ec52ef599f4f7dbc5cc8fbc7d1e1d7b630
armindeck <dbhsoficial2003@gmail.com>
2025-05-27 13:41:03 -0500

    Se cambio la función mensajeSpan() y se adapto para que use los archivos json

--- app/global/usuario.php
+++ app/global/usuario.php
@@ -1,37 +1,32 @@
 <?php
 if(isset($_SESSION['id']) || isset($_SESSION['rol'])){
-	if(!file_exists($Web['directorio'].AppDatabase('usuarios/usuarios'))){
+	if(!file_exists(DATA->UserRoute())){
 		unset($_SESSION['id']); unset($_SESSION['rol']);
-		mensajeSpan(['bg'=>'red',
-            'text'=> Language('user-not-found', 'alert'),
-            'ruta'=>"{$Web['directorio']}auth/registrar{$Web['config']['php']}"
-        ]);
+		sendAlert->Error(Language('user-not-found', 'alert'),
+        	"{$Web['directorio']}auth/register{$Web['config']['php']}"
+        );
 	}
-	require $Web['directorio'].AppDatabase('usuarios/usuarios');
+	$usu = DATA->User();
 	if(!isset($usu[$_SESSION['id']]) or !isset($usu[$_SESSION['id']]['rol'])){
 		unset($_SESSION['id']); unset($_SESSION['rol']);
-		mensajeSpan(['bg'=>'red',
-            'text'=> Language('user-not-exist', 'alert'),
-            'ruta'=>"{$Web['directorio']}auth/iniciar{$Web['config']['php']}"
-        ]);
+		sendAlert->Error(Language('user-not-exist', 'alert'),
+        	"{$Web['directorio']}auth/login{$Web['config']['php']}"
+        );
 	}
 
-	if(file_exists($Web['directorio'].AppDatabase('usuarios/usuarios_extras'))){
-		require $Web['directorio'].AppDatabase('usuarios/usuarios_extras');
+	if(file_exists(DATA->UserRoute(true))){
+		$usu = DATA->UserAll();
 		if($usu[$_SESSION['id']]['estado'] != 'publico'){
 			unset($_SESSION['id']); unset($_SESSION['rol']);
-			mensajeSpan(['bg'=>'red',
-                'text'=> Language('user-suspended-deleted', 'alert'),
-                'ruta'=>"{$Web['directorio']}auth/iniciar{$Web['config']['php']}"
-            ]);
+			sendAlert->Error(Language('user-suspended-deleted', 'alert'),
+            	"{$Web['directorio']}auth/login{$Web['config']['php']}"
+            );
 		}
 	}
 	if($_SESSION['rol'] != $usu[$_SESSION['id']]['rol']){
 		unset($_SESSION['id']); unset($_SESSION['rol']);
-        mensajeSpan(['bg'=>'red',
-            'text'=> Language('role-mismatch', 'alert'),
-            'ruta'=>"{$Web['directorio']}auth/iniciar{$Web['config']['php']}"
-        ]);
+        sendAlert->Error(Language('role-mismatch', 'alert'),
+        	"{$Web['directorio']}auth/login{$Web['config']['php']}"
+        );
 	}
 }
-?>
