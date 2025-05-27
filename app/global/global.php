a5b8df81254be441b3376c4cc7061988ec2468d0
armindeck <dbhsoficial2003@gmail.com>
2025-05-27 13:45:09 -0500

    Nuevos cambios

--- app/global/global.php
+++ app/global/global.php
@@ -1,20 +1,20 @@
 <?php require_once __DIR__ . '/get.php';
 foreach ([
-	'usuario', 'routes/auth', 'routes/panel'
+	'usuario', 'routes/auth', 'routes/admin', 'routes/api'
 	] as $value) { require_once __DIR__ . "/{$value}.php"; }
-if ($Web['ruta_completa'] == '../p/perfil.php') { header("Location: ../auth/iniciar.php"); }
+if ($Web['ruta_completa'] == '../p/perfil.php') { header("Location: ../auth/login.php"); }
 
 Ruta(null,
-		$Web['ruta_completa'] != '../panel/panel.php' &&
-		$Web['ruta_completa'] != '../panel/procesa/procesa.creador.borrador.php',
+		$Web['ruta_completa'] != '../admin/admin.php' &&
+		$Web['ruta_completa'] != '../admin/process/creator.php',
 	function () use ($Web) {
 		if(!isset($_SESSION['rol']) or isset($_SESSION['rol']) && strtolower($_SESSION['rol']) != 'ceo founder'){
 			CrearCarpetas('database/other/');
-			$file = $Web['directorio'].'database/other/count.json';
-			$leer = Database("other/count");
+			$file = $Web['directorio'].'database/other/visits.json';
+			$leer = DATA->Read("other/visits");
 			$leer['total'] = AumentarJSON($leer, 'total');
 			$leer[$Web['ruta']] = AumentarJSON($leer, $Web['ruta']);
 
-			file_put_contents($file, json_encode($leer));
+			DATA->Save("other/visits", $leer);
 		}
 }); ?>
