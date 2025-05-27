7b3b073b1e83d0961d1fc2c038d6d1ea1fc67c02
armindeck <dbhsoficial2003@gmail.com>
2025-05-27 14:00:38 -0500

    Anuncio

--- app/views/components/anuncio-nav-view.php
+++ app/views/components/anuncio-nav-view.php
@@ -1,19 +1,19 @@
-<?php if($Web['ruta_completa'] != '../panel/panel.php'): global $AX;
+<?php if($Web['ruta_completa'] != '../admin/admin.php'): global $AX;
 if(isset($AX['anuncio']) && !empty($AX['anuncio'])) {
 	echo '<center>';
-	if (isset($Web['anuncios']['mostrar_anuncio_mensaje_movimiento']) && !empty($Web['anuncios']['mostrar_anuncio_mensaje_movimiento'])){ ?>
+	if (isset($Web['ads']['mostrar_anuncio_mensaje_movimiento']) && !empty($Web['ads']['mostrar_anuncio_mensaje_movimiento'])){ ?>
 		<div class="anuncio-marquee">
-			<a target="_blank" href="<?= $Web['anuncios']['enlace_anuncio_mensaje_movimiento'] ?? '' ?>">
+			<a target="_blank" href="<?= $Web['ads']['enlace_anuncio_mensaje_movimiento'] ?? '' ?>">
 				<marquee direction="left" onmouseout="start();" onmouseover="stop();" scrollamount="10" scrolldelay="145">
-					<?= $Web['anuncios']['anuncio_mensaje_movimiento_texto'] ?? '' ?>
+					<?= $Web['ads']['anuncio_mensaje_movimiento_texto'] ?? '' ?>
 				</marquee>
 			</a>
 		</div>
 	<?php }
-	if (isset($Web['anuncios']['mostrar_anuncio_mini_banner']) && !empty($Web['anuncios']['mostrar_anuncio_mini_banner'])) { ?>
+	if (isset($Web['ads']['mostrar_anuncio_mini_banner']) && !empty($Web['ads']['mostrar_anuncio_mini_banner'])) { ?>
 		<div class="anuncio-mini-banner">
-			<a target="_blank" href="<?= $Web['anuncios']['enlace_anuncio_mini_banner'] ?? '' ?>">
-				<img loading="lazy" src="<?= $Web['config']['https_imagen']; ?><?= $Web['anuncios']['imagen_anuncio_mini_banner'] ?? '' ?>">
+			<a target="_blank" href="<?= $Web['ads']['enlace_anuncio_mini_banner'] ?? '' ?>">
+				<img loading="lazy" src="<?= $Web['config']['https_imagen']; ?><?= $Web['ads']['imagen_anuncio_mini_banner'] ?? '' ?>">
 			</a>
 		</div>
 		<hr>
