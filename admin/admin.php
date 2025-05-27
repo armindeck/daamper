128bee27722e74a1d3273658818a8d7b00f0a9f7
armindeck <dbhsoficial2003@gmail.com>
2025-05-27 13:06:11 -0500

    Ahora admin/ sera el ex-apartado panel/

---
+++ admin/admin.php
@@ -0,0 +1,3 @@
+<?php # 25/05/2025 - 3:34pm
+$Web = ['directorio'=>'../','ruta'=>'admin/admin.php'];
+require_once $Web['directorio'].'app/controller/controller.php';
---
+++ admin/index.php
@@ -0,0 +1,3 @@
+<?php # 23/05/2025 - 11:48am
+$Web = ['directorio'=>'../','ruta'=>'admin/index.php'];
+require_once $Web['directorio'].'app/controller/controller.php';
